28c9151f485de7f7e6cb4ef0ed725918bb7ebb70
Paweł <84644073+Pawel-Lechki@users.noreply.github.com>
2024-01-15 19:41:49 +0100

    Add body of class Przesylka

--- Kolokwium1/model/Kurier.cs
+++ Kolokwium1/model/Kurier.cs
@@ -2,5 +2,36 @@
 
 public class Kurier
 {
-    
+    private string id;
+    private string opis;
+    private bool dostarczona;
+    private double masa;
+
+
+    public Kurier(string id, string opis, bool dostarczona)
+    {
+        this.id = id;
+        this.opis = opis;
+        this.dostarczona = false;
+    }
+
+    public bool czyDostarczona()
+    {
+        return this.dostarczona;
+    }
+
+    public void dostarcz()
+    {
+        this.dostarczona = true;
+    }
+
+    public virtual double waga()
+    {
+        return this.masa;
+    }
+
+    public virtual string info()
+    {
+        return $"Przesyła id: {this.id}, zawiera: {this.opis}, waga: {this.waga()}";
+    }
 }
